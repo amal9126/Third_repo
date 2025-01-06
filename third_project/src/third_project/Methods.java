930885e8f78927f15c16f4cfd55412a3884c9e75
AMAL RAJ <AMAL RAJ@LAPTOP-92ICR69A>
2025-01-06 23:49:36 +0530

    Third commit

---
+++ third_project/src/third_project/Methods.java
@@ -0,0 +1,24 @@
+package third_project;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Methods.addition();
+Methods obj=new Methods();
+obj.multiply();
+	}
+public static void addition()
+{
+	int a=5,b=6,sum;
+	sum=a+b;
+	System.out.println(sum);
+}
+
+public void multiply()
+{
+int c=6,d=2,result;
+result=c*d;
+System.out.println(result);
+}
+}
